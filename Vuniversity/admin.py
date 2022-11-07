1d0fd273f62b592f7431ccdab53669ea67f625cf
ahmad allahham <117184203+geassgate@users.noreply.github.com>
2022-11-07 15:29:59 +0200

    Delete Vuniversity directory

--- Vuniversity/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Student)
-admin.site.register(Lecture)
-admin.site.register(Lecturer)
-admin.site.register(Corse)
--- Vuniversity/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VuniversityConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Vuniversity'
